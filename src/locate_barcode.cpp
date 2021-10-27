5533cd8095aa6fdab5e7ef28e466111b42c819f3
Mitchell <mitch.torok@gmail.com>
2021-10-26 21:22:50 -0700

    Added multiple subscribers, range just needs to be found

--- src/locate_barcode.cpp
+++ src/locate_barcode.cpp
@@ -7,58 +7,85 @@
 #include "geometry_msgs/msg/point.hpp"
 #include "zbar_ros_interfaces/msg/symbol.hpp"
 #include "cv_bridge/cv_bridge.h"
+#include <sensor_msgs/msg/laser_scan.hpp>
 
 #include <string>
 
 using std::placeholders::_1;
 using namespace std;
 
-int x = 0;
 
-std::string markers[10] = {"apple","2","3","","5","","7","","9",""};
 
-class MinimalSubscriber : public rclcpp::Node
-{
+std::string markers[10] = {"","","","","","","","","",""};
+
+
+
+class MinimalSubscriber : public rclcpp::Node {
+	
+
 public:
-
-  MinimalSubscriber()
-  : Node("minimal_subscriber")
-  {  
-
+  
+  MinimalSubscriber() : Node("minimal_subscriber") {  
     subscription_ = this->create_subscription<zbar_ros_interfaces::msg::Symbol>("barcode", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
     
+    scanSub_ = this->create_subscription<sensor_msgs::msg::LaserScan>("scan", 1, std::bind(&MinimalSubscriber::callbackScan, this, std::placeholders::_1));
+    
+    
     publisher_ = this->create_publisher<geometry_msgs::msg::PointStamped>("topic", 10);
+    
+    xpos = 0;
+    publishPoint = 0;
     }
 
 private:
-  void topic_callback(const zbar_ros_interfaces::msg::Symbol::SharedPtr msg) const
-  {
-    //RCLCPP_INFO(this->get_logger(), "Points: '%f'", msg->points[0].x);
-    RCLCPP_INFO(this->get_logger(), "Data: '%s'", msg->data.c_str());
-    x ++ ;
-    RCLCPP_INFO(this->get_logger(), "X: '%d'", x);
+  int xpos;
+  int publishPoint;
+  
+  void callbackScan(const sensor_msgs::msg::LaserScan::SharedPtr scan) {
+
+  	if (publishPoint == 1) {
+  		RCLCPP_INFO(this->get_logger(), "min data: '%d' ", xpos);
+  		RCLCPP_INFO(this->get_logger(), "laser data: '%f' ", scan->ranges[0]);
+  		
+  		
+  		//SORT HERE
+  	    	geometry_msgs::msg::PointStamped pointStamped;
+    		pointStamped.header.frame_id = "base_link";
+    		
+	  	pointStamped.point.x = 0.0;
+	    	pointStamped.point.y = 0.0;
+	    	pointStamped.point.z = 0.0;
+	    	
+	    	publishPoint = 0;
+	    	publisher_->publish(pointStamped);
+    	}
+  }
+  
+  void topic_callback(const zbar_ros_interfaces::msg::Symbol::SharedPtr msg)  {
+
+     int valueOne = msg->points[0].x ;
+     int ValueTwo = msg->points[1].x;
+        
+     xpos = (valueOne+ValueTwo)/2;
+    //for (int i = 0; i < 10; i ++ ) {
+    //	RCLCPP_INFO(this->get_logger(), "i: '%d' string: '%s'", i,markers[i].c_str());
+    //}
     
     
-    markers[2] = "lets hope this gets replaced";
-    for (int i = 0; i < 10; i ++ ) {
-    	RCLCPP_INFO(this->get_logger(), "i: '%d' string: '%s'", i,markers[i].c_str());
+    for ( int i = 0; i < 10; i ++) {
+    	//If marker is already in list we don't need to publish again
+        if (msg->data.compare(markers[i]) == 0) { break; }
+    	
+    	//If reached end of list add marker then find and broadcast point
+    	if (markers[i].compare("") == 0) {
+    		markers[i] = msg->data.c_str();
+    		publishPoint = 1;
+    		break;
+    	}
     }
-    
-    if (msg->data.compare(markers[0]) == 0) {
-    	RCLCPP_INFO(this->get_logger(), "It worky goods");
-    }
-    
-    
-    //top left point:
-    //RCLCPP_INFO(this->get_logger(), "Points: '%f'", msg->points[0].x);
-    //RCLCPP_INFO(this->get_logger(), "Points: '%f'", msg->points[0].y);
-    
-    //top right point?:
-    //RCLCPP_INFO(this->get_logger(), "Points: '%f'", msg->points[1].x);
-    //RCLCPP_INFO(this->get_logger(), "Points: '%f'", msg->points[1].y);
-    
     
   }
+  rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr scanSub_;
   rclcpp::Subscription<zbar_ros_interfaces::msg::Symbol>::SharedPtr subscription_;
   rclcpp::Publisher<geometry_msgs::msg::PointStamped>::SharedPtr publisher_;
 
